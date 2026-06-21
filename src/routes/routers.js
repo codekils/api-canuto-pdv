1492bb659cce294a7460ebfb22b31e28aff3fa70
codekils <fernandes.trbl@gmail.com>
2026-06-20 21:10:31 -0300

    feat(routes) new routes

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -1,7 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 const loginhandler = require("../controllers/loginHandler");
+const {
+  createProductHanddler,
+  getProductByIdHandler,
+  getProductByNameHanddler,
+  getAllProductsHanddler,
+  deleteProductHanddler,
+  updateProductHanddler,
+} = require("../controllers/productHandler");
 
 router.post("/login", loginhandler);
+router.post("/produto", createProductHanddler);
+router.get("/produto/busca", getProductByNameHanddler);
+router.get("/produto/:id", getProductByIdHandler);
+router.get("/produto", getAllProductsHanddler);
+router.delete("/produto/:id", deleteProductHanddler);
+router.patch("/produto/:id", updateProductHanddler);
 
 module.exports = router;
