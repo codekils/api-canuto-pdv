18270af42126199dd289c8b41be9eab5be4180c9
codekils <fernandes.trbl@gmail.com>
2026-06-20 21:16:23 -0300

    style(src) changed name servidor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const router = require("./routes/routers");
 require("dotenv").config();
 const PORT = process.env.PORT || 3030;
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
